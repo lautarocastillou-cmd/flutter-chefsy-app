afc9eba5eb93095b266f4865d59f44e7c5e18132
lautarocastillou-cmd <lautarocastillou@gmail.com>
2026-07-25 23:23:50 -0300

    fix(build): fijar flutter-version 3.29.0 en Shorebird, AGP 8.7.3 y gradle 8.10.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "xyz.chefsy.chefsy_cadete"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 24
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
